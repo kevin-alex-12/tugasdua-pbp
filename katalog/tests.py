4693b4f7aa45cb0f13e3d1d860799062b3a295c2
Kevin Alexander <kevinalexande26@gmail.com>
2022-09-14 13:56:26 +0700

    Add tests.py

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class AppTest(TestCase):
+    def katalog_url_exists(self):
+        response = Client().get('/katalog/')
+        self.assertEqual(response.status_code, 200)
+
+    def katalog_template_test(self):
+        response = Client().get('/katalog/')
+        self.assertTemplateUsed(response, 'katalog.html')
