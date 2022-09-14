63575523764388cc1db6e8a98d4849c4cd980fd8
Kevin Alexander <kevinalexande26@gmail.com>
2022-09-14 17:51:16 +0700

    Membuat style.css dan menjalankan python manage.py collectstatic

--- katalog/tests.py
+++ katalog/tests.py
@@ -3,6 +3,5 @@ from django.test import TestCase, Client
 # Create your tests here.
 class KatalogTest(TestCase):
     def test_katalog_url_exists(self):
-        # Akan menghasilkan error karena style.css memang belum dibuat
         response = Client().get('/katalog/')
         self.assertEqual(response.status_code, 200)
