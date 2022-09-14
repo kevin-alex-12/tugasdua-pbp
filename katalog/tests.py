2c3a773c998965d449588c42763ada372267dee1
Kevin Alexander <kevinalexande26@gmail.com>
2022-09-14 17:14:56 +0700

    Update test.py

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,11 +1,8 @@
 from django.test import TestCase, Client
 
 # Create your tests here.
-class AppTest(TestCase):
-    def katalog_url_exists(self):
+class KatalogTest(TestCase):
+    def test_katalog_url_exists(self):
+        # Akan menghasilkan error karena style.css memang belum dibuat
         response = Client().get('/katalog/')
         self.assertEqual(response.status_code, 200)
-
-    def katalog_template_test(self):
-        response = Client().get('/katalog/')
-        self.assertTemplateUsed(response, 'katalog.html')
