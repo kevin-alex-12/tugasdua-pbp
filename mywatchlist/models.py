d08da1eaff908119a0d784006c1e8e7ec2150c7b
Kevin Alexander <kevinalexande26@gmail.com>
2022-09-20 19:03:57 +0700

    Fixing Bugs #2

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class MyWatchList(models.Model):
-    watched = models.BooleanField(default=True)
+    watched = models.BooleanField()
     title = models.CharField(max_length=100)
     rating = models.IntegerField()
-    release_date = models.TextField()
+    release_date = models.DateField()
     review = models.TextField()
